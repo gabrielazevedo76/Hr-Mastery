b82ce83e4f843e151aeb6f44bfbf8f02c09c46f6
Gabriel <shigetora@DESKTOP-QSM5H3L.Dlink>
2022-03-27 21:09:42 -0300

    Dont allow duplicated resumes

--- App/models.py
+++ App/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Prevent duplicated emails
+class Registered_email(models.Model):
+    email = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.email
--- App/views.py
+++ App/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect # Redirect the page after submit
 from django.contrib import messages # Return messages
 from django.core.mail import EmailMultiAlternatives # Requered to send emails
 from django.template import loader # Render templates on email body
+from App.models import Registered_email # Informations in models.py
 
 # Function to render home page
 def home(request):
@@ -18,101 +19,140 @@ def opportunities(request):
 # Function to send frontend form
 def email_frontend(request):
     if request.method == "POST":
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        address = request.POST.get('address')
-        experience = request.POST.get('experience')
-        skills = request.POST.get('skills')
-    
-        template = loader.get_template('resume_form.txt')
-        context = {
-            'name' : name,
-            'age' : age,
-            'email' : email,
-            'phone' : phone,
-            'address' : address,
-            'experience' : experience,
-            'skills' : skills,
-        }
-        message = template.render(context)
-        email = EmailMultiAlternatives(
-            "Frontend - Candidate", message,
-            "Frontend Opportunity",
-            ['bestofdjango@gmail.com',]
-        )
-        email.content_subtype = 'html'
-        file = request.FILES['file']
-        email.attach(file.name, file.read(), file.content_type)
-        email.send()
-        messages.success(request, 'Frontend resume sent successfully!')
-        return HttpResponseRedirect('/')
+
+        #Check if email exist in DB
+        email = request.POST['email']
+
+        if Registered_email.objects.filter(email=email).exists():
+            messages.error(request, "We already have your resume in our DB!")
+            return HttpResponseRedirect("/opportunities")
+        else:
+            name = request.POST.get('name')
+            age = request.POST.get('age')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            address = request.POST.get('address')
+            experience = request.POST.get('experience')
+            skills = request.POST.get('skills')
+
+            # Register inside DB
+            contact = Registered_email()
+            contact.email = email
+            contact.save()
+        
+            template = loader.get_template('resume_form.txt')
+            context = {
+                'name' : name,
+                'age' : age,
+                'email' : email,
+                'phone' : phone,
+                'address' : address,
+                'experience' : experience,
+                'skills' : skills,
+            }
+            message = template.render(context)
+            email = EmailMultiAlternatives(
+                "Frontend - Candidate", message,
+                "Frontend Opportunity",
+                ['bestofdjango@gmail.com',]
+            )
+            email.content_subtype = 'html'
+            file = request.FILES['file']
+            email.attach(file.name, file.read(), file.content_type)
+            email.send()
+            messages.success(request, 'Frontend resume sent successfully!')
+            return HttpResponseRedirect('/')
 
 # Function to send backend form
 def email_backend(request):
-    if request.method == "POST":
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        address = request.POST.get('address')
-        experience = request.POST.get('experience')
-        skills = request.POST.get('skills')
-    
-        template = loader.get_template('resume_form.txt')
-        context = {
-            'name' : name,
-            'age' : age,
-            'email' : email,
-            'phone' : phone,
-            'address' : address,
-            'experience' : experience,
-            'skills' : skills,
-        }
-        message = template.render(context)
-        email = EmailMultiAlternatives(
-            "Backend - Candidate", message,
-            "Backend Opportunity",
-            ['bestofdjango@gmail.com',]
-        )
-        email.content_subtype = 'html'
-        file = request.FILES['file']
-        email.attach(file.name, file.read(), file.content_type)
-        email.send()
-        messages.success(request, 'Backend resume sent successfully!')
-        return HttpResponseRedirect('/')
+
+    #Check if email exist in DB
+    email = request.POST['email']
+
+    if Registered_email.objects.filter(email=email).exists():
+        messages.error(request, "We already have your resume in our DB!")
+        return HttpResponseRedirect("/opportunities")
+    else:
+        if request.method == "POST":
+            name = request.POST.get('name')
+            age = request.POST.get('age')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            address = request.POST.get('address')
+            experience = request.POST.get('experience')
+            skills = request.POST.get('skills')
+
+            # Register inside DB
+            contact = Registered_email()
+            contact.email = email
+            contact.save()
+        
+            template = loader.get_template('resume_form.txt')
+            context = {
+                'name' : name,
+                'age' : age,
+                'email' : email,
+                'phone' : phone,
+                'address' : address,
+                'experience' : experience,
+                'skills' : skills,
+            }
+            message = template.render(context)
+            email = EmailMultiAlternatives(
+                "Backend - Candidate", message,
+                "Backend Opportunity",
+                ['bestofdjango@gmail.com',]
+            )
+            email.content_subtype = 'html'
+            file = request.FILES['file']
+            email.attach(file.name, file.read(), file.content_type)
+            email.send()
+            messages.success(request, 'Backend resume sent successfully!')
+            return HttpResponseRedirect('/')
 
 # Function to send fullstack form
 def email_fullstack(request):
-    if request.method == "POST":
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        address = request.POST.get('address')
-        experience = request.POST.get('experience')
-        skills = request.POST.get('skills')
-    
-        template = loader.get_template('resume_form.txt')
-        context = {
-            'name' : name,
-            'age' : age,
-            'email' : email,
-            'phone' : phone,
-            'address' : address,
-            'experience' : experience,
-            'skills' : skills,
-        }
-        message = template.render(context)
-        email = EmailMultiAlternatives(
-            "Fullstack - Candidate", message,
-            "Fullstack Opportunity",
-            ['bestofdjango@gmail.com',]
-        )
-        email.content_subtype = 'html'
-        file = request.FILES['file']
-        email.attach(file.name, file.read(), file.content_type)
-        email.send()
-        messages.success(request, 'Fullstack resume sent successfully!')
-        return HttpResponseRedirect('/')
+
+    #Check if email exist in DB
+    email = request.POST['email']
+
+    if Registered_email.objects.filter(email=email).exists():
+        messages.error(request, "We already have your resume in our DB!")
+        return HttpResponseRedirect("/opportunities")
+    else:
+        if request.method == "POST":
+            name = request.POST.get('name')
+            age = request.POST.get('age')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            address = request.POST.get('address')
+            experience = request.POST.get('experience')
+            skills = request.POST.get('skills')
+        
+            # Register inside DB
+            contact = Registered_email()
+            contact.email = email
+            contact.save()
+
+            template = loader.get_template('resume_form.txt')
+            context = {
+                'name' : name,
+                'age' : age,
+                'email' : email,
+                'phone' : phone,
+                'address' : address,
+                'experience' : experience,
+                'skills' : skills,
+            }
+            message = template.render(context)
+            email = EmailMultiAlternatives(
+                "Fullstack - Candidate", message,
+                "Fullstack Opportunity",
+                ['bestofdjango@gmail.com',]
+            )
+            email.content_subtype = 'html'
+            file = request.FILES['file']
+            email.attach(file.name, file.read(), file.content_type)
+            email.send()
+            messages.success(request, 'Fullstack resume sent successfully!')
+            return HttpResponseRedirect('/')
